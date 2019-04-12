5d11689f09720d5ce765107ad6bb3d98a20186a1
Andre Janßen <andre.janssen@kratzer-automation.com>
2019-04-12 17:50:49 +0200

    Bootstrapping an Angular App

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: '<b>Hello</b>',
+})
+export class EventsAppComponent {
+  title = 'Andres super App';
+}
