759f9b654aed07fadb7afd527f16c882c33db6ed
Andre Janßen <andre.janssen@kratzer-automation.com>
2019-05-03 09:24:36 +0200

    populated event card

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -5,10 +5,34 @@ import { Component} from '@angular/core'
     template: `
     <div>
         <h1>Upcoming Angular Events</h1>
+        <hr>
+        <div class="well hoverwell thumbnail">
+            <h2>{{event.name}}</h2>
+            <div>Date: {{event.date}}</div>
+            <div>Time: {{event.time}}</div>
+            <div>Price: \${{event.price}}</div>
+            <div>
+                <span>Location: {{event.location.address}}</span>
+                <span>&nbsp;</span>
+                <span>Location: {{event.location.city}}, {{event.location.country}}</span>
+            </div>
+        </div>
     </div>
     `
 })
 
 export class EventsListComponent {
-
+    event = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '01.01.2070',
+        time: '15:00',
+        price: 123.45,
+        imageUrl: '/assets/images/angularconnect-shield.png',
+        location: {
+                address: 'Some Street',
+                city: 'A Town',
+                county: 'Somewhere'
+        }
+    }
 }
