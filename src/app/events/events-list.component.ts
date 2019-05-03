6c0fa6ca4b49c0933dc3cf7322fab5fd42a8edbb
Andre Janßen <andre.janssen@kratzer-automation.com>
2019-05-03 09:43:01 +0200

    added child component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,11 +2,31 @@ import { Component} from '@angular/core'
 
 @Component({
     selector:'events-list',
-    templateUrl: './events-list.component.html'
+    template: `
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr>
+        <event-thumbnail [event]="event1"></event-thumbnail>
+        <event-thumbnail [event]="event1"></event-thumbnail>
+    </div>
+    `
 })
 
 export class EventsListComponent {
-    event = {
+    event1 = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '01.01.2070',
+        time: '15:00',
+        price: 123.45,
+        imageUrl: '/assets/images/angularconnect-shield.png',
+        location: {
+                address: 'Some Street',
+                city: 'A Town',
+                county: 'Somewhere'
+        }
+    }
+    event2 = {
         id: 1,
         name: 'Angular Connect',
         date: '01.01.2070',
