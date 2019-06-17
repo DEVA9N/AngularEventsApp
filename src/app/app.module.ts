5f3c0efe89055ebae7a6dd4e75a0669ddd6620c7
Andre Janßen <andre.janssen@kratzer-automation.com>
2019-06-17 17:31:22 +0200

    organized imports with barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { 
+  EventListComponent,
+  CreateEventComponent,
+  EventDetailsComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventListResolver,
+  EventRouteActivator,
+} from './events/index' 
 import { EventsAppComponent } from './events-app.component';
-import { EventListComponent } from './events/event-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event-service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/event-list-resolver.service';
 
 @NgModule({
   imports: [
