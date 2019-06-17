703a6a7826699c34849ec278b1edbdea078070b6
Andre Janßen <andre.janssen@kratzer-automation.com>
2019-06-17 17:14:49 +0200

    renamed routes to app.routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { EventService } from './events/shared/event-service';
 import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
-import { appRoutes } from './routes';
+import { appRoutes } from './app.routes';
 import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
