313ec329b68422a7e5ab567b1beb07781fed01d1
Andre Janßen <andre.janssen@kratzer-automation.com>
2020-01-10 12:55:34 +0100

    Routing in the same component (fixed search result selection)

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core'
 import { EventService } from '../shared/event.service'
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { IEvent, ISession } from '../shared';
+import { resetComponentState } from '@angular/core/src/render3/state';
 
 @Component({
     templateUrl: './event-details.component.html',
@@ -14,7 +15,7 @@ import { IEvent, ISession } from '../shared';
 
 export class EventDetailsComponent {
     event: IEvent
-    addMode : boolean
+    addMode: boolean
     filterBy: string = "all"
     sortBy: string = "votes"
 
@@ -23,20 +24,28 @@ export class EventDetailsComponent {
     }
 
     ngOnInit() {
-        this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
+        this.route.params.forEach((params: Params) => {
+            this.event = this.eventService.getEvent(+params['id'])
+
+            this.resetState()
+        })
+    }
+
+    resetState(){
+        this.addMode = false
+        this.filterBy = "all"
+        this.sortBy = "votes"
     }
 
     addSession() {
         this.addMode = true
-
-
     }
 
-    cancelAddSession(){
-        this.addMode=false
+    cancelAddSession() {
+        this.addMode = false
     }
 
-    saveNewSession(session:ISession) {
+    saveNewSession(session: ISession) {
         const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id))
 
         session.id = nextId + 1
